f3334f6b31dd952d4fa16cec4f66c7d1f0121b00
nemnous <nani.botta.123@gmail.com>
2020-04-04 01:08:41 +0530

    Major Changes - Checkstyle, project Structure, InputReader

--- src/AbstractClassModel/AbstractAccount.java
+++
@@ -1,7 +0,0 @@
-package AbstractClassModel;
-
-public abstract class AbstractAccount {
-	public abstract void deposit(float cash);
-	public abstract boolean withdraw(float cash);
-	public abstract float getAvailableBalance();
-}
